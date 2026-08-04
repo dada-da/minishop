b74ba4378c024f435acff15de03b49dc332bb5ed
Cao Duc Anh <caoducanh.1501@gmail.com>
2026-08-04 23:21:30 +0700

    :feat: MS-17 Add entity order and order item

--- src/main/java/dev/dada/minishop/order/Order.java
+++ src/main/java/dev/dada/minishop/order/Order.java
@@ -3,6 +3,9 @@ package dev.dada.minishop.order;
 import dev.dada.minishop.common.BaseEntity;
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * TASK MS-17: Entity Order (user, items @OneToMany, status, totalAmount, snapshot dia chi).
  * Luu y: OrderItem phai snapshot price tai thoi diem dat (gia product co the doi sau).
@@ -14,4 +17,17 @@ public class Order extends BaseEntity {
 
     @Column(name = "user_id",nullable = false)
     private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<OrderItem> orderItems;
+
+    @Column(name = "total_Amount", nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(name = "address", nullable = false)
+    private String address;
 }
--- src/main/java/dev/dada/minishop/order/OrderItem.java
+++ src/main/java/dev/dada/minishop/order/OrderItem.java
@@ -1,8 +1,11 @@
 package dev.dada.minishop.order;
 
 import dev.dada.minishop.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import dev.dada.minishop.product.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigInteger;
 
 /**
  * TASK MS-17: OrderItem (order, productId, productName snapshot, unitPrice snapshot, quantity).
@@ -11,4 +14,24 @@ import jakarta.persistence.Table;
 @Table(name = "order_items")
 public class OrderItem extends BaseEntity {
     // TODO MS-17
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    @Positive
+    private Integer quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    @Positive
+    private BigInteger unitPrice;
+
+    @Column(name = "unit_original_price", nullable = false)
+    @Positive
+    private BigInteger unitOriginalPrice;
 }
